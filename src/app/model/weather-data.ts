d57eec340392e2123a928c41617027d37f1c62af
Jorciney <jorciney.dias@hotmail.com>
2023-11-25 14:04:27 +0100

    adding weather data model

---
+++ src/app/model/weather-data.ts
@@ -0,0 +1,104 @@
+/** https://www.visualcrossing.com/ is used for the weather api */
+interface WeatherData {
+  queryCost: number;
+  latitude: number;
+  longitude: number;
+  resolvedAddress: string;
+  address: string;
+  timezone: string;
+  tzoffset: number;
+  description: string;
+  days: Day[];
+  alerts: unknown[];
+  stations: { [key: string]: Station };
+  currentConditions: CurrentConditions;
+}
+
+interface Day {
+  datetime: string;
+  datetimeEpoch: number;
+  tempmax: number;
+  tempmin: number;
+  temp: number;
+  feelslikemax: number;
+  feelslikemin: number;
+  feelslike: number;
+  dew: number;
+  humidity: number;
+  precip: number;
+  precipprob: number;
+  precipcover: number;
+  preciptype: string[];
+  snow: number;
+  snowdepth: number;
+  windgust: number;
+  windspeed: number;
+  winddir: number;
+  pressure: number;
+  cloudcover: number;
+  visibility: number;
+  solarradiation: number;
+  solarenergy: number;
+  uvindex: number;
+  severerisk: number;
+  sunrise: string;
+  sunriseEpoch: number;
+  sunset: string;
+  sunsetEpoch: number;
+  moonphase: number;
+  conditions: string;
+  description: string;
+  icon: string;
+  stations: string[];
+  source: string;
+  hours: Hour[];
+}
+
+interface Hour {
+  datetime: string;
+  datetimeEpoch: number;
+  temp: number;
+  feelslike: number;
+  humidity: number;
+  dew: number;
+  precip: number;
+  precipprob: number;
+  snow: number;
+  snowdepth: number;
+  preciptype: string[];
+  windgust: number;
+  windspeed: number;
+  winddir: number;
+  pressure: number;
+  visibility: number;
+  cloudcover: number;
+  solarradiation: number;
+  solarenergy: number;
+  uvindex: number;
+  severerisk: number;
+  conditions: string;
+  icon: string;
+  stations: string[];
+  source: string;
+}
+
+interface Station {
+  distance: number;
+  latitude: number;
+  longitude: number;
+  useCount: number;
+  id: string;
+  name: string;
+  quality: number;
+  contribution: number;
+}
+
+interface CurrentConditions extends Omit<Hour, 'datetime' | 'datetimeEpoch'> {
+  datetime: string;
+  datetimeEpoch: number;
+  sunrise: string;
+  sunriseEpoch: number;
+  sunset: string;
+  sunsetEpoch: number;
+  moonphase: number;
+}
