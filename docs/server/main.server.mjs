d4d6b5b68e555645683c25d2b1274ac3379465ac
Jorciney <jorciney.dias@hotmail.com>
2023-11-27 10:29:48 +0100

    building for github-pages

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WLX3ENOK.mjs";import"./chunk-AERIM2GF.mjs";import"./chunk-5WD6OWNH.mjs";import"./chunk-O3QL7HJD.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
