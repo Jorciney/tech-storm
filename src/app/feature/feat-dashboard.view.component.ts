4b59c95a03e19a5cfa7e5f75772dbe86407f677c
Jorciney <jorciney.dias@hotmail.com>
2023-11-28 21:29:22 +0100

    adding icon to main screen

--- src/app/feature/feat-dashboard.view.component.ts
+++ src/app/feature/feat-dashboard.view.component.ts
@@ -2,16 +2,20 @@ import { Component, inject, OnInit } from '@angular/core';
 import { WeatherFacade } from '../data-access/+state/weather.facade';
 import {TodaysForecastComponent} from "./todays-forecast.component";
 import {LetDirective} from "@ngrx/component";
-import {UpperCasePipe} from "@angular/common";
+import {DecimalPipe, NgOptimizedImage, UpperCasePipe} from "@angular/common";
+import {map} from "rxjs";
 
 @Component({
   standalone: true,
   selector: 'tech-storm-dashboard-view',
   template: `
       <div *ngrxLet="weatherData$; let weather" class="h-full w-full flex flex-row justify-center items-center">
-          <div class="flex flex-col justify-center items-center">
+          <div class="flex flex-col justify-center items-center" *ngrxLet="currentWeather$; let currentConditions">
               <div>DashboardView works!</div>
               <div class="font-extrabold text-3xl">{{weather?.address | uppercase}}</div>
+              <div class="font-extrabold text-3xl">{{currentConditions?.temp | number: '1.0-0'}} °</div>
+              <img [ngSrc]="'assets/icons/animated/' + currentConditions?.icon + '.svg'" [width]="200" [height]="150"
+                   alt="weather-icon"/>
               <tech-storm-todays-forecast></tech-storm-todays-forecast>
           </div>
       </div>
@@ -19,12 +23,15 @@ import {UpperCasePipe} from "@angular/common";
   imports: [
     TodaysForecastComponent,
     LetDirective,
-    UpperCasePipe
+    UpperCasePipe,
+    DecimalPipe,
+    NgOptimizedImage
   ]
 })
 export class DashboardViewComponent implements OnInit {
   private readonly weatherFacade = inject(WeatherFacade);
   weatherData$ = this.weatherFacade.weatherData$;
+  currentWeather$ = this.weatherData$.pipe(map(weatherData => weatherData?.currentConditions));
 
   ngOnInit(): void {
     this.weatherFacade.loadWeatherData('zemst');
