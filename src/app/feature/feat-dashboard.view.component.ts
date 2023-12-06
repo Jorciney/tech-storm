e09ae71944263c6c8d7005751c7242f20ba1391e
Jorciney <jorciney.dias@hotmail.com>
2023-12-06 08:15:06 +0100

    fixed null pointer error

--- src/app/feature/feat-dashboard.view.component.ts
+++ src/app/feature/feat-dashboard.view.component.ts
@@ -2,28 +2,29 @@ import { Component, inject, OnInit } from '@angular/core';
 import { WeatherFacade } from '../data-access/+state/weather.facade';
 import { TodaysForecastComponent } from './todays-forecast.component';
 import { LetDirective } from '@ngrx/component';
-import { DecimalPipe, NgOptimizedImage, UpperCasePipe } from '@angular/common';
+import {DecimalPipe, NgIf, NgOptimizedImage, UpperCasePipe} from '@angular/common';
 import { map } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'tech-storm-dashboard-view',
   template: `
-    <div *ngrxLet="weatherData$; let weather" class="h-full w-full flex flex-row justify-center items-center">
-      <div class="flex flex-col justify-center items-center" *ngrxLet="currentWeather$; let currentConditions">
-        <div>DashboardView works!</div>
-        <div class="font-extrabold text-3xl">{{ weather?.address | uppercase }}</div>
-        <div class="font-extrabold text-3xl">{{ currentConditions?.temp | number : '1.0-0' }} °</div>
-        <img
-          [ngSrc]="'assets/icons/animated/' + currentConditions?.icon + '.svg'"
-          [width]="200"
-          [height]="150"
-          alt="weather-icon" />
-        <tech-storm-todays-forecast></tech-storm-todays-forecast>
+    <div *ngrxLet="weatherData$; let weather" class="h-full w-full flex flex-col justify-center items-center">
+      <div class="flex flex-row items-center justify-between gap-5" *ngrxLet="currentWeather$; let currentConditions">
+        <div>
+          <div class="font-extrabold text-3xl">{{ weather?.address | uppercase }}</div>
+          <div class="font-extrabold text-3xl">{{ currentConditions?.temp | number : '1.0-0' }} °</div>
+        </div>
+        <img *ngIf="currentConditions?.icon"
+            [ngSrc]="'assets/icons/animated/' + currentConditions?.icon + '.svg'"
+            [width]="200"
+            [height]="150"
+            alt="weather-icon"/>
       </div>
+      <tech-storm-todays-forecast></tech-storm-todays-forecast>
     </div>
   `,
-  imports: [TodaysForecastComponent, LetDirective, UpperCasePipe, DecimalPipe, NgOptimizedImage],
+  imports: [TodaysForecastComponent, LetDirective, UpperCasePipe, DecimalPipe, NgOptimizedImage, NgIf],
 })
 export class DashboardViewComponent implements OnInit {
   private readonly weatherFacade = inject(WeatherFacade);
