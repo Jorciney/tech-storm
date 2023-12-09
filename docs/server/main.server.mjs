ddd419dbf3e5a7e4738cf95e510bc1dc79f558d9
Jorciney <jorciney.dias@hotmail.com>
2023-12-09 11:37:56 +0100

    building pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-G27XJYOR.mjs";import"./chunk-PUDITRML.mjs";import"./chunk-CZBECRZ7.mjs";import"./chunk-RPWBRWTO.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-VJM36P4Q.mjs";import"./chunk-DJZCPCJ6.mjs";import"./chunk-BCJQ2GVK.mjs";import"./chunk-TQ42HCT7.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
