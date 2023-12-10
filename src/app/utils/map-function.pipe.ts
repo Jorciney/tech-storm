a5be14d5e2f1062884aa74ebb28da961cdf4850f
Jorciney <jorciney.dias@hotmail.com>
2023-12-10 21:20:36 +0100

    added mapFunctionPipe

---
+++ src/app/utils/map-function.pipe.ts
@@ -0,0 +1,11 @@
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+  name: 'mapToFunction',
+  standalone: true,
+})
+export class MapFunctionPipe<T> implements PipeTransform{
+    transform(value: unknown, funtionToMapTo: Function): T {
+        return funtionToMapTo(value);
+    }
+}
