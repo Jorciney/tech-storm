d367b0e2cb5b0136a004068b8f29d46da7c52c2b
Jorciney <jorciney.dias@hotmail.com>
2023-12-12 15:28:30 +0100

    removing logs

--- src/app/data-access/weather.service.ts
+++ src/app/data-access/weather.service.ts
@@ -1,17 +1,14 @@
-import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { WeatherData } from './model/weather-data';
-import {tap} from "rxjs";
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {WeatherData} from './model/weather-data';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class WeatherService {
   private readonly httpClient = inject(HttpClient);
 
   getWeatherData(location: string) {
-    console.log('WeatherService.getWeatherData', location);
     return this.httpClient.get<WeatherData>(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=9AU959UYVABKBXGYWQGJFAAAN&contentType=json`,
-    ).pipe(
-      tap(response => console.log("logging response", response)),
-    )};
+    )
+  };
 }
