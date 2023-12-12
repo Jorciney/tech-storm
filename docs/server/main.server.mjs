6730e280bd4a442972990d306580a92f5569d6d2
Jorciney <jorciney.dias@hotmail.com>
2023-12-12 16:30:57 +0100

    pages

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UIALKIGQ.mjs";import"./chunk-TAPF7JGK.mjs";import"./chunk-H2SDIYUY.mjs";import"./chunk-TH2NBBEF.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
---
+++ docs/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{h as o,i as r,j as n}from"./chunk-TAPF7JGK.mjs";import{ob as e}from"./chunk-TH2NBBEF.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
